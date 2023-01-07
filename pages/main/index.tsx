bec72ab5e02f1742618a297b67de81581b766a07
resur <sol.reshur@gmail.com>
2023-01-07 22:35:42 +0900

    main : MainFrame에 Home Components 추가

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,13 +1,15 @@
 import * as MainStyles from '../../styles/main.styles';
 import Nav from '../src/components/units/nav/Nav.Container';
-
+import Home from '../src/components/units/home/Home.Container';
 export default function MainPage() {
   return (
     <MainStyles.Frame>
       <MainStyles.NavBar>
         <Nav />
       </MainStyles.NavBar>
-      <MainStyles.MainFrame></MainStyles.MainFrame>
+      <MainStyles.MainFrame>
+        <Home />
+      </MainStyles.MainFrame>
     </MainStyles.Frame>
   );
 }
