c93c368c9f55f5a6977a18688f1d909abeacc554
resur <sol.reshur@gmail.com>
2023-01-07 22:42:54 +0900

    commons: 공통 style.ts

---
+++ pages/src/components/commons/Styles.ts
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const PageTitle = styled.div`
+  font-size: 62px;
+  font-family: 'Chivo Mono', monospace;
+  font-weight: 400;
+  margin-bottom: 50px;
+  color: #616161;
+`;
+export const TittleAttention = styled.span`
+  font-family: 'Chivo Mono', monospace;
+  color: #ff8a80;
+`;
+export const LastUpdate = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+  color: #616161;
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
+`;
