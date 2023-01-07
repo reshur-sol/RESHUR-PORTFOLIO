2387d7103a7ede5891644dec6f40ab8cfb5eb35f
resur <sol.reshur@gmail.com>
2023-01-07 22:34:36 +0900

    document : title,webfont,meta 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='ko'>
-      <Head />
+      <Head>
+        <meta
+          name='viewport'
+          content='viewport-fit=cover'
+        />
+        <meta
+          name='description'
+          content="RESHUR's Portfolio"
+        />
+        <meta
+          name='author'
+          content='RESHUR'
+        />
+        <meta charSet='utf-8'></meta>
+        <link
+          rel='icon'
+          href='/favicon.ico'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Chivo+Mono:wght@300;400;600;700&family=Roboto+Mono:wght@300;400;500;600;700&family=Rock+Salt&display=swap'
+          rel='stylesheet'
+        />
+        <title>HELLO, I'M RESHUR</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
