264a3a81620844698835ff81b210b27e164a78b1
resur <sol.reshur@gmail.com>
2023-01-12 15:12:49 +0900

    cotainer: Header 추가 및 Navbar 추가

---
+++ pages/src/components/container/Container.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Nav from '../units/nav/Nav.Container';
+
+export default function Container() {
+  return (
+    <>
+      <Head>
+        <title>HELLO, I'M RESHUR</title>
+      </Head>
+      <header>
+        <Nav />
+      </header>
+      <main></main>
+    </>
+  );
+}
